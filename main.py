cd49ce34d6d80d8c7094780d1f11adb9ea99a66c
Jun Xiong Tan <t0916977@kelaberetiv.sg>
2019-05-08 19:01:25 +0800

    test deployment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 from flask import Flask
+from generate_unconditional_samples import sample_model
 
 app = Flask(__name__)
 
@@ -12,8 +13,8 @@ def hello():
 @app.route('/showmethemoney')
 def showmethemoney():
     """Show me the money"""
-    subfile_output = subprocess.run('python3 src/generate_unconditional_samples.py --nsamples 1 --length 25 --temperature 0.7 --top_k 30', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-    return subfile_output
+#    subfile_output = subprocess.run('python3 src/generate_unconditional_samples.py --nsamples 1 --length 25 --temperature 0.7 --top_k 30', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return sample_model(nsamples=1, length=25, temperature=0.7, top_k=35)
 
 @app.errorhandler(500)
 def server_error(e):
