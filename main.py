bf17ddf23db49876982bf62ccd437ae403de6f2c
Jun Xiong Tan <t0916977@kelaberetiv.sg>
2019-05-08 19:25:02 +0800

    test deployment

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import logging
 import subprocess
 from flask import Flask
-from generate_unconditional_samples import sample_model
+import fire
+import json
+import os
+import numpy as np
+import tensorflow as tf
+import model, sample, encoder
+#from generate_unconditional_samples import sample_model
 
 app = Flask(__name__)
 
@@ -10,11 +16,11 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
-@app.route('/showmethemoney')
-def showmethemoney():
-    """Show me the money"""
+# @app.route('/showmethemoney')
+# def showmethemoney():
+#     """Show me the money"""
 #    subfile_output = subprocess.run('python3 src/generate_unconditional_samples.py --nsamples 1 --length 25 --temperature 0.7 --top_k 30', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-    return sample_model(nsamples=1, length=25, temperature=0.7, top_k=35)
+#    return sample_model(nsamples=1, length=25, temperature=0.7, top_k=35)
 
 @app.errorhandler(500)
 def server_error(e):
