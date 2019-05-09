9301fbeecd2b53c972bb4a47af241071087672ad
Jun Xiong Tan <t0916977@kelaberetiv.sg>
2019-05-10 00:49:28 +0800

    NOW TRUMP TALKS

---
+++ interact_model.py
@@ -0,0 +1,58 @@
+import json
+import os
+import numpy as np
+import tensorflow as tf
+
+import model, sample, encoder
+
+def interact_model(
+    model_name='117M',
+    seed=None,
+    nsamples=1,
+    batch_size=1,
+    length=50,
+    temperature=0.85,
+    top_k=40,
+    raw_text=''
+):
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join('models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k
+        )
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        saver.restore(sess, ckpt)
+
+        while True:
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            text = ''
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text += enc.decode(out[i])
+    return text
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import sample_model
+import interact_model
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
@@ -17,7 +18,8 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(sample_model.sample_model())
+    update.message.reply_text(interact_model.interact_model(raw_text=update.message.text))
+#    update.message.reply_text(sample_model.sample_model())
 #    update.message.reply_text(update.message.text)
 
 def error(update, context):
