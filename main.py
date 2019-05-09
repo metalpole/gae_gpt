6ec345552ac1795f1e3f84ec4e88317a8a0548e0
Jun Xiong Tan <t0916977@kelaberetiv.sg>
2019-05-10 00:30:28 +0800

    deploy on gce

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import logging
+import sample_model
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
-
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
+    update.message.reply_text(sample_model.sample_model())
+#    update.message.reply_text(update.message.text)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -56,7 +52,6 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
